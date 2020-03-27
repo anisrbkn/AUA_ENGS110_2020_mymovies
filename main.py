1d0433e8bc57545f3a8c87d25423492dee33d87e
anisrbkn <61308905+anisrbkn@users.noreply.github.com>
2020-03-27 21:58:50 +0400

    Update and rename code.py to main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+user = input('please log in:')
+print("Welcome")
+
+import json
+with open("movie_info.json") as f:
+    data = json.load(f)
+for movie_info in data ['movies']:
+    print(movie_info)
+
+genre = input('select a genre:')
+release_year = input('select release_year:')
+for movies in data['movies']:
+  if(movies["genre"] == (genre)):
+     print(movies['movie_name'])
+for movies in data['movies']:
+  if(movies["release_year"] == (release_year)):
+      print(movies['movie_name'])
