2cec9a839458b07372857bf542258788566a31cb
anisrbkn <61308905+anisrbkn@users.noreply.github.com>
2020-03-27 22:41:45 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ print("Welcome")
 import json
 with open("movies_info.json") as f:
     data = json.load(f)
-for movie_info in data ['movies']:
-    print(movie_info)
+for movies_info in data ['movies']:
+    print(movies_info)
 
 genre = input('select a genre:')
 release_year = input('select release_year:')
